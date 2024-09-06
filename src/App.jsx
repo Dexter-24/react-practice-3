d509e7be0616b21fadea587a97cb1c178d661dd9
Yuliia Vorobei <vorobeju1@gmail.com>
2024-09-06 21:53:33 +0200

    task 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { lazy, Suspense } from "react";
-import { Header, Loader } from "./components";
-import { Routes, Route, Navigate } from "react-router-dom";
-const Home = lazy(() => import("pages/Home"))
-const Country = lazy(() => import("pages/Country"))
-const SearchCountry = lazy(() => import("pages/SearchCountry"))
+import { lazy, Suspense } from 'react';
+import { Header, Loader } from './components';
+import { Routes, Route, Navigate } from 'react-router-dom';
+const Home = lazy(() => import('pages/Home'));
+const Country = lazy(() => import('pages/Country'));
+const SearchCountry = lazy(() => import('pages/SearchCountry'));
 
 export const App = () => {
   return (
     <>
       <Header />
-      <Suspense fallback={<Loader/>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/country" element={<SearchCountry />} />
           <Route path="/country/:countryId" element={<Country />} />
-          <Route path="*" element={<Navigate to="/"/> } />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
     </>
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,49 @@
-import { Container, Heading, Section } from 'components';
+import {
+  Container,
+  CountryInfo,
+  GoBackBtn,
+  Heading,
+  Loader,
+  Section,
+} from 'components';
+import { useEffect, useRef, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/countryApi';
 
 const Country = () => {
+  const [country, setCountry] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { countryId } = useParams();
+  const location = useLocation();
+  console.log(location, 'country');
+  const goBack = useRef(location?.state ?? '/');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const data = await fetchCountry(countryId);
+        setCountry(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [countryId]);
+
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <GoBackBtn path={goBack.current}>Back to countries</GoBackBtn>
+        {country && <CountryInfo {...country} />}
+        {loading && <Loader />}
+        {error && (
+          <Heading title="Ooop...its some error..please try again" bottom />
+        )}
       </Container>
     </Section>
   );
