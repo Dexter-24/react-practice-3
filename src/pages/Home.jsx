39c38be100c23732c9d28508f90daaebb62d4b6b
Taisiia <159428493+Taisiia-Mazur@users.noreply.github.com>
2024-09-06 21:25:11 +0300

    task2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,39 @@
-import { Container, Heading, Section } from 'components';
+
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useState, useEffect } from 'react';
+import { getCountries } from 'service/countryApi';
 
 const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(null);
+const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+try {
+  const data = await getCountries()
+  setCountries(data);
+  
+} catch (error) {
+  setError(error);
+} finally {
+  setLoading(false);
+}
+    }
+    fetchData();
+  }, [])
+  
+
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        {countries.length > 0 && <CountryList countries={countries} />}
+        {loading && <Loader />}
+        {error && <Heading title="Ooop...its some error..please try again" bottom />}
+        
       </Container>
     </Section>
   );
